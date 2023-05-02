d3f06d5d0ac781709ebc59d7813356c9a8d2c41a
James Sullivan <jsullivan8794@gmail.com>
2023-05-01 23:38:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`
+};
+saturdayFun('Bathe my dog');
+
+const mondayWork = function(work = 'go to the office') {
+        return `This Monday, I will ${work}.`
+};
+mondayWork('work from home')
+
+function wrapAdjective (val1 = '*') {
+    return function (val2 = 'special') {
+        return `You are ${val1}${val2}${val1}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
